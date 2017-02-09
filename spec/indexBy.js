e7c5d0b9141945af223801b01172555d46ca0a71
Briana Brown <brownbb1990@gmail.com>
2017-02-09 14:42:40 -0800

    added remaing test files

--- spec/indexBy.js
+++ spec/indexBy.js
@@ -2,8 +2,8 @@ import chai, {expect} from 'chai'
 import chaiChange from 'chai-change'
 import indexBy from '../src/indexBy'
 
-let array = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
-  describe('indexBy()', function() {
+let dopeArray = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
+  describe.only('indexBy()', function() {
     it('should return -1 when the value is not present', function() {
       expect(indexBy({name: 'moe' , age: 40})).to.equal(0 , 0)
     });
