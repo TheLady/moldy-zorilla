4c877c4fff3e06785cc433c77c7c85f191adafa3
thelady <dihaynes77@gmail.com>
2017-02-09 14:12:59 -0800

    added contains function, added clearer names to tests

---
+++ spec/contains.test.js
@@ -0,0 +1,32 @@
+import chai, {expect} from 'chai'
+import chaiChange from 'chai-change'
+import contains from '../src/contains'
+
+
+
+const radArray = [1,2,3,4,5]
+describe('#contains()', function() {
+  it.only('should return true when the element is present in the array', function() {
+    expect(contains(4, radArray)).to.eql(true)
+  });
+
+  it('should return false when the element is not present in the array', function() {
+    expect(contains(6, radArray)).to.eql(false)
+  });
+});
+
+//returns false if the element doesn't exist in the array
+//  const array = [1,2,3,4,5]
+//    describe.only('#contains()', function() {
+//     it('should return false when the object is not present', function() {
+//       expect(contains(7, [1,2,3,4,5])).to.eql(3)
+//      });
+//    });
+
+
+
+// contains_.isElement(object)
+// Returns true if object is a DOM element.
+
+// _.isElement(jQuery('body')[0]);
+// => true
---
+++ src/contains.js
@@ -0,0 +1,25 @@
+const contains = (element, array) => {
+  for( let i = 0; i < array.length; i++) {
+    if (array[i] === element) {return true}
+  }
+  return false
+}
+
+
+//if loop finishes without finding the element, return false.
+export default contains
+
+
+
+
+
+// const contains = (element, array) => {
+//   for( let i = 0; i < array.length; i++) {
+//     if (i == true) {
+//       return true}
+//       }
+//     }
+
+
+
+// export default contains
